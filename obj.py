51f073831a813a421b93badfa289a645630fb833
Melvin Lim <melvinlim@MacBook.local>
2018-05-11 17:25:57 -0700

    extract parameters properly

--- obj.py
+++ obj.py
@@ -4,7 +4,13 @@ class Obj():
 		self.genN=genN
 		data=raw.strip('<<')
 		data=data.split('>>')
-		self.params=data[0].split('\n')
+		params=data[0].split('\n')
+		params=filter(None,params)
+		self.params=dict()
+		for words in params:
+			tmp=filter(None,words)
+			tmp=tmp.split(' ')
+			self.params[tmp[0]]=tmp[1:]
 		self.data=[]
 		if len(data)>1:
 			for words in data[1:]:
