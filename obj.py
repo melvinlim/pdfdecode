00b38d6245768ed23051e40f4795884ecaa8a0d4
Melvin Lim <melvinlim@MacBook.local>
2018-05-11 19:08:55 -0700

    extract font table information

--- obj.py
+++ obj.py
@@ -1,12 +1,37 @@
+def extractTokens(raw):
+	start=raw.find('<<')+2
+	end=raw.rfind('>>')
+	if start<0 or end<0:
+		return raw
+	tmp=raw[start:end]
+	return tmp
 class Obj():
 	def __init__(self,objN,genN,raw):
 		self.objN=int(objN)
 		self.genN=int(genN)
+		x=extractTokens(raw)
 		data=raw.strip('<<')
 		data=data.strip('\n')
 		data=data.split('>>')
 		params=data[0].split('\n')
 		self.params=dict()
+		self.isFontTable=False
+		fontIndex=data[0].find('/Font')
+		if fontIndex>=0:
+			tmp=data[0][fontIndex:]
+			if tmp.find('<<')>=0:
+#				print tmp.split(' ')
+				if tmp.split(' ')[1].strip('\n')=='<<':
+					tmp=extractTokens(tmp)
+					print fontIndex
+					print tmp
+					tmp=tmp.split(' ')
+					n=len(tmp)
+					i=0
+					while(i<n):
+						self.params[tmp[i]]=tmp[i+1:i+4]
+						i+=4
+					self.isFontTable=True
 		filterIndex=data[0].find('/Filter')
 		if filterIndex>=0:
 			tmp=data[0][filterIndex:].split('\n')
