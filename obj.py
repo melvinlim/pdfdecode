63db192fc30b878d26ffc53402d288b4f59e9a14
Melvin Lim <melvinlim@MacBook.local>
2018-05-11 17:53:21 -0700

    use zlib decompression if compressed.

--- obj.py
+++ obj.py
@@ -3,6 +3,7 @@ class Obj():
 		self.objN=objN
 		self.genN=genN
 		data=raw.strip('<<')
+		data=data.strip('\n')
 		data=data.split('>>')
 		params=data[0].split('\n')
 		params=filter(None,params)
@@ -12,8 +13,21 @@ class Obj():
 			tmp=filter(None,tmp)
 			self.params[tmp[0]]=tmp[1:]
 		self.data=[]
-		if len(data)>1:
-			for words in data[1:]:
-				tmp=words.strip('\n')
-				if tmp!='':
-					self.data.append(tmp)
+		if len(data)==2 and data[1]!='' and data[1]!='\n':
+			tmp=data[1]
+			if tmp.find('endstream')>=0:
+				tmp=tmp.strip('\n')
+				tmp=tmp.strip('endstream')
+				tmp=tmp.strip('stream')
+				tmp=tmp.strip('\n')
+			if '/Filter' in self.params and '/FlateDecode' in self.params['/Filter']:
+				import zlib
+				try:
+					self.data=zlib.decompress(tmp)
+				except:
+					print 'zlib failed header check'
+					self.data=tmp
+			else:
+				self.data=tmp
+		else:
+			self.data=raw
